a7877a5658953ac47e7c99568bd6600f26886055
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-08-29 02:34:14 +0000

    did more things

--- gridexp.py
+++ gridexp.py
@@ -1,2 +1,9 @@
-def board():
-    
+def create_grid(rows, cols):
+    return [[9 for _ in range(cols)] for _ in range(rows)]
+
+grid = create_grid(int(input("Enter number of rows:")), int(input("Enter number of columns:")))
+
+for row in grid:
+    for item in row:
+        print(item, end=' ')
+    print()
