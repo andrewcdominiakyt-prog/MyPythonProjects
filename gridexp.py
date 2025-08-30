79e5bbe8663dfb4fd5d851dbb54df0b8ed0be6ad
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-08-30 01:37:16 +0000

    help

--- gridexp.py
+++ gridexp.py
@@ -1,3 +1,12 @@
+
+import pygame
+import time
+import random
+
+gamerunning = False
+
+
+
 def create_grid(rows, cols):
     return [["#" for _ in range(cols)] for _ in range(rows)]
 
@@ -7,30 +16,49 @@ def print_grid(grid):
             print(item, end=' ')
         print()
 
-def modify_grid(grid, r, c):
-    if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-        grid[r][c] = "x"
-    else:
-        print(f"Skipping invalid cell ({r}, {c})")
 
-rows = int(input("Enter number of rows: "))
-cols = int(input("Enter number of columns: "))
-grid = create_grid(rows, cols)
 
-r = 0
-c = 0
+def activatesquare(y,x):
+    grid[y][x] = "X"
 
-for i in range(rows * cols):
-    modify_grid(grid, r, c)
+def deactivatesquare(y,x):
+    grid[y][x]= "#"
+
+grid = create_grid(5, 5)
+
+activex = 2
+activey = 2
+
+
+def gametick():
+    global activey, activex
+    grid[activey][activex] = "X"
     print_grid(grid)
-    print()
+    direction = input("Enter direction (w/a/s/d) or 'q' to quit: ")
+    if direction == 'w' and activey > 0:
+        deactivatesquare(activey, activex)
+        activey -= 1
+        activatesquare(activey, activex)
+    elif direction == 's' and activey < 4:
+        deactivatesquare(activey, activex)
+        activey += 1
+        activatesquare(activey, activex)
+    elif direction == 'a' and activex > 0:
+        deactivatesquare(activey, activex)
+        activex -= 1
+        activatesquare(activey, activex)
+    elif direction == 'd' and activex < 4:
+        deactivatesquare(activey, activex)
+        activex += 1
+        activatesquare(activey, activex)
+    elif direction == 'q':
+        print("Exiting game.")
+        global gamerunning
+        gamerunning = False
+    else:
+        print("Invalid move. Try again.")
 
-    c += 1
-    if c >= cols:  
-        c = 0
-        r += 1
-
-    if r >= rows: 
-        print("Reached end of grid.")
-        break
-
+gamerunning = True
+while gamerunning == True:
+    gametick()
+    
