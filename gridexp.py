f343f6873f5fc21c7a1c597b70cde053ffc7663d
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-08-30 15:18:56 +0000

    works just gotta add gamelogic

--- gridexp.py
+++ gridexp.py
@@ -1,4 +1,3 @@
-
 import time
 import random
 
@@ -14,63 +13,67 @@ def print_grid(grid):
         for item in row:
             print(item, end=' ')
         print()
+    print()
 
 def apple():
-    global appleonboard
+    global appleonboard, grid
+    if appleonboard:
+        return
     while True:
-        if appleonboard == False:
-            y = random.randint(0, 4)
-            x = random.randint(0, 4)
-            if grid[y][x] == "#":
-                grid[y][x] = "A"
-                appleonboard = True
-            else: 
-                continue
-#error is somewhere here i need to figurre how to fix this
+        y = random.randint(0, 4)
+        x = random.randint(0, 4)
+        if grid[y][x] == "#":
+            grid[y][x] = "A"
+            appleonboard = True
+            break
 
-
-def activatesquare(y,x):
+def activatesquare(y, x):
     grid[y][x] = "X"
 
-def deactivatesquare(y,x):
-    grid[y][x]= "#"
+def deactivatesquare(y, x):
+    grid[y][x] = "#"
 
 grid = create_grid(5, 5)
 
 activex = 2
 activey = 2
 
-
 def gametick():
+    global activey, activex, gamerunning, score, appleonboard
+
     apple()
 
-    global activey, activex
-    grid[activey][activex] = "X"
     print_grid(grid)
     direction = input("Enter direction (w/a/s/d) or 'q' to quit: ")
+
+    ny, nx = activey, activex
     if direction == 'w' and activey > 0:
-        deactivatesquare(activey, activex)
-        activey -= 1
-        activatesquare(activey, activex)
+        ny -= 1
     elif direction == 's' and activey < 4:
-        deactivatesquare(activey, activex)
-        activey += 1
-        activatesquare(activey, activex)
+        ny += 1
     elif direction == 'a' and activex > 0:
-        deactivatesquare(activey, activex)
-        activex -= 1
-        activatesquare(activey, activex)
+        nx -= 1
     elif direction == 'd' and activex < 4:
-        deactivatesquare(activey, activex)
-        activex += 1
-        activatesquare(activey, activex)
+        nx += 1
     elif direction == 'q':
         print("Exiting game.")
-        global gamerunning
         gamerunning = False
+        return
     else:
         print("Invalid move. Try again.")
+        return
+
+    if (ny, nx) != (activey, activex):
+        if grid[ny][nx] == "A":
+            score += 1
+            appleonboard = False  
+            print("Score:", score)
+
+        deactivatesquare(activey, activex)
+        activey, activex = ny, nx
+        activatesquare(activey, activex)
 
 gamerunning = True
-while gamerunning == True:
+activatesquare(activey, activex)  
+while gamerunning:
     gametick()
