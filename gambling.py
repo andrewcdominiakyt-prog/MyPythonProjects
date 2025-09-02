d7759f0fbb8d7033b8fb0b5595b835f9fad36234
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-09-02 15:43:28 +0000

    added basic gambling :happy:

--- gambling.py
+++ gambling.py
@@ -1,1 +1,14 @@
 
+import random
+
+#make difficulty settings before gamestart
+difficulty = 0.5
+
+# Initialize the grid
+gridsize = (int(input("Enter Desired Grid Size:")))
+grid = [['#' for _ in range(gridsize)] for _ in range(gridsize)]
+def print_grid(grid):
+    for row in grid:
+        print (" ".join(row))
+
+numbombs = round((gridsize * gridsize) * difficulty)
